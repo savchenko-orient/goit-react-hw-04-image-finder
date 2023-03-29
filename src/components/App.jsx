a86704702c73889259271c5064147269ff03c462
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-03-29 20:08:49 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,22 +11,15 @@ const perPage = 12;
 
 export default function App() {
 
-  let [images, setImages] = useState([]);
+  const [images, setImages] = useState([]);
   const [query, setQuery] = useState('');
-  let [page, setPage] = useState(1);
-  let [totalHits, setTotalHits] = useState(0);
-  let [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
   const [isOpenModal, setIsOpenModal] = useState(false)
   const [imgLargeSrc, setImgLargeSrc] = useState(null);
   const [imgAlt, setImgAlt] = useState(null);
 
-  const loadImages = (response) => {
-    setImages(() => images = response.hits)
-    setTotalHits(() => totalHits = response.totalHits)
-  }
-  const loadMoreImages = (response) => {
-    setImages((images) => [...images, ...response.hits]);
-  }
 
   useEffect(() => {
     if (query === '') return
@@ -37,11 +30,12 @@ export default function App() {
         const response = await fetchImages(query, page);
 
         if (page === 1) {
-          loadImages(response);
+          setImages(response.hits)
+          setTotalHits(response.totalHits)
         }
 
         if (page >= 2) {
-          loadMoreImages(response)
+          setImages((images) => [...images, ...response.hits]);
         }
       } catch (error) {
         throw new Error(error);
